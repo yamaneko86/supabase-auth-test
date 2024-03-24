d02c749c14c833d098dae7d6ca74659cf3ac148c
yamaneko86 <yamaneko8699@gmail.com>
2024-03-24 15:28:54 +0900

    init commit

--- components/AuthButton.tsx
+++ components/AuthButton.tsx
@@ -3,35 +3,38 @@ import Link from "next/link";
 import { redirect } from "next/navigation";
 
 export default async function AuthButton() {
-  const supabase = createClient();
+	const supabase = createClient();
 
-  const {
-    data: { user },
-  } = await supabase.auth.getUser();
+	const {
+		data: { user },
+	} = await supabase.auth.getUser();
 
-  const signOut = async () => {
-    "use server";
+	const signOut = async () => {
+		"use server";
 
-    const supabase = createClient();
-    await supabase.auth.signOut();
-    return redirect("/login");
-  };
+		const supabase = createClient();
+		await supabase.auth.signOut();
+		return redirect("/login");
+	};
 
-  return user ? (
-    <div className="flex items-center gap-4">
-      Hey, {user.email}!
-      <form action={signOut}>
-        <button className="py-2 px-4 rounded-md no-underline bg-btn-background hover:bg-btn-background-hover">
-          Logout
-        </button>
-      </form>
-    </div>
-  ) : (
-    <Link
-      href="/login"
-      className="py-2 px-3 flex rounded-md no-underline bg-btn-background hover:bg-btn-background-hover"
-    >
-      Login
-    </Link>
-  );
+	return user ? (
+		<div className="flex items-center gap-4">
+			Hey, {user.email}!
+			<form action={signOut}>
+				<button
+					type="submit"
+					className="py-2 px-4 rounded-md no-underline bg-btn-background hover:bg-btn-background-hover"
+				>
+					Logout
+				</button>
+			</form>
+		</div>
+	) : (
+		<Link
+			href="/login"
+			className="py-2 px-3 flex rounded-md no-underline bg-btn-background hover:bg-btn-background-hover"
+		>
+			Login
+		</Link>
+	);
 }
